ccc4eec58446eb73d08f01707a5d71dcf191c7c7
alemendieta017 <amendieta017@gmail.com>
2022-07-08 19:55:40 -0400

    Delete and add product with Alert

---
+++ src/services/api/product.js
@@ -0,0 +1,26 @@
+import axios from 'axios'
+import endPoints from '@services/api'
+
+const addProduct = async (body) => {
+  const config = {
+    headers: {
+      accept: '*/*',
+      'Content-type': 'application/json',
+    },
+  }
+
+  const response = await axios.post(
+    endPoints.products.addProducts,
+    body,
+    config
+  )
+
+  return response.data
+}
+
+const deleteProduct = async (id) => {
+  const response = await axios.delete(endPoints.products.deleteProduct(id))
+  return response.data
+}
+
+export { addProduct, deleteProduct }
